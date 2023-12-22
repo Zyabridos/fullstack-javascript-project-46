28cf2b7ffd2460ca807a41750142a0fc972eb5c9
Nina Zyabrina <zyabrina95@gmail.com>
2023-12-22 14:37:02 +0100

    makeAstTree cleaned

--- src/makeAstTree.js
+++ src/makeAstTree.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const makeAstTree = (data1, data2) => _.sortBy(_.union(_.keys(data1), _.keys(data2)))
+export default (data1, data2) => _.sortBy(_.union(_.keys(data1), _.keys(data2)))
   .map((key) => {
     const value = data1[key];
     const Value = data2[key];
@@ -41,65 +41,3 @@ const makeAstTree = (data1, data2) => _.sortBy(_.union(_.keys(data1), _.keys(dat
       status: 'unchanged',
     };
   });
-
-const file1 = {
-  common: {
-    setting1: 'Value 1',
-    setting2: 200,
-    setting3: true,
-    setting6: {
-      key: 'value',
-      doge: {
-        wow: '',
-      },
-    },
-  },
-  group1: {
-    baz: 'bas',
-    foo: 'bar',
-    nest: {
-      key: 'value',
-    },
-  },
-  group2: {
-    abc: 12345,
-    deep: {
-      id: 45,
-    },
-  },
-};
-
-const file2 = {
-  common: {
-    follow: false,
-    setting1: 'Value 1',
-    setting3: null,
-    setting4: 'blah blah',
-    setting5: {
-      key5: 'value5',
-    },
-    setting6: {
-      key: 'value',
-      ops: 'vops',
-      doge: {
-        wow: 'so much',
-      },
-    },
-  },
-  group1: {
-    foo: 'bar',
-    baz: 'bars',
-    nest: 'str',
-  },
-  group3: {
-    deep: {
-      id: {
-        number: 45,
-      },
-    },
-    fee: 100500,
-  },
-};
-
-const result = makeAstTree(file1, file2);
-console.log(JSON.stringify(result));
